41f0c184affa9655d551384a54038e2ac05a5f72
dhamina <dhaminas@gmail.com>
2021-05-30 17:38:47 +0530

    manifest

--- pages/_document.js
+++ pages/_document.js
@@ -17,8 +17,8 @@ class MyDocument extends Document {
 					<meta name='keywords' content='Keywords' />
 					<title>Next.js PWA Example</title>
 					<link rel="manifest" href="/manifest.json" />
-					<link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-					<link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+					<link href='/logo/icon-192x192.png' rel='icon' type='image/png' sizes='192x192' />
+					<link href='/logo/icon-256x256.png' rel='icon' type='image/png' sizes='256x256' />
 					<link rel="apple-touch-icon" href="/apple-icon.png"></link>
 					<meta name="theme-color" content="#317EFB"/>
 		</Head>
