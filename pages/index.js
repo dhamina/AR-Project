ef49a2d5e5a9bc7417d8759777412058954efec4
samanvi19 <36392465+samanvi19@users.noreply.github.com>
2021-06-15 20:21:20 +0530

    priya login

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,8 +14,13 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         Wecome to AR Books
+         Welcome to AR Books
+        
+        
         </h1>
+        <Link href="/login">
+        <a  className={styles.title}>Login</a>
+         </Link>
       </main>
         
     </div>
