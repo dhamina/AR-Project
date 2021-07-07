e38454bafc7e599cdeebd97d54f53f560e0c3d43
samanvi19 <36392465+samanvi19@users.noreply.github.com>
2021-07-08 00:26:24 +0530

    Concepts UI

--- pages/books/[id]/index.js
+++ pages/books/[id]/index.js
@@ -1,12 +1,48 @@
 export default function ListOfConcepts(props) {
     console.log(props)
-    return(
-        <div>Concepts page</div>
+    return (
+
+
+        <div>
+
+            <h1>Textbook - Physics</h1>
+            <div className="list__cn">
+            <div className="list__cn-item"> Gravity  </div>
+            <div className="list__cn-item"> Heat </div>
+            <div className="list__cn-item"> Wave  </div>
+</div>
+            <style jsx>
+                {
+                    `
+                    .list__cn{
+                        display: grid;
+                        grid-template-columns: 45% 45%;
+                        padding:10px;
+                    }
+                    .list__cn-item{
+                        width: 95%;
+                         margin: 10px;
+                        border: 1px solid grey;
+                        background: white;
+                        border-radius: 12px;
+                        height:300px;
+                        display:flex;
+                        justify-content:center;
+                        height: 100px;
+                        align-items: center;
+                    }
+            
+              
+            `
+                }
+            </style>
+        </div>
+
     )
 }
 ListOfConcepts.getInitialProps = async (ctx) => {
-    const {id} = ctx.query
-    return{
+    const { id } = ctx.query
+    return {
         id
     }
 }
